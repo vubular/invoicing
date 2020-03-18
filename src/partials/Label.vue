8489d48b6a5eef34eee5441c66bf478552104d56
Mentor Gashi <m.gashi@albismart.com>
2020-03-18 16:58:43 +0100

    Label icon gap

--- src/partials/Label.vue
+++ src/partials/Label.vue
@@ -2,7 +2,7 @@
 	<div v-if="showLabel" class="column">
 		<h3 class="subtitle is-3 has-text-grey-light has-text-weight-light">
 			<span class="icon"><i class="fal fa-th-large"></i></span>
-			<span v-html="label"></span>
+			<span v-html="label" style="margin-left:15px"></span>
 		</h3>
 	</div>
 </template>
