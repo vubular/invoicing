4bb7494364c32eb0da42e17789c72c1474901dbe
Mentor Gashi <m.gashi@albismart.com>
2020-03-20 18:42:50 +0100

    Minor fixes

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -1,6 +1,9 @@
 <template>
-	<invoice-table-view :fields="fields"
+	<invoice-table-view :vat="vat"
+						:show="show"
+						:fields="fields"
 						:features="features"
+						:customer="customer"
 						:content="content"></invoice-table-view>
 </template>
 <script>
@@ -9,8 +12,11 @@
 		name: "InvoiceContent",
 		components: { InvoiceTableView },
 		props: {
+			vat: { type: Object, },
+			show: { type: Boolean, },
 			fields: { type: String, },
 			features: { type: String, },
+			customer: { type: Object, },
 			content: {
 				type: Array,
 				default() {
