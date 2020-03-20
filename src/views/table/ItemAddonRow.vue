e9eef11b96563f3a1d17229623cf416854865b12
Mentor Gashi <m.gashi@albismart.com>
2020-03-21 00:17:32 +0100

    Removing props, set, remove methods, initial dynamic inputs approach, show/edit state fields.

--- src/views/table/ItemAddonRow.vue
+++ src/views/table/ItemAddonRow.vue
@@ -2,76 +2,85 @@
 	<tr v-if="addon">
 		<td class="has-text-grey has-text-weight-light">{{counter}}</td>
 		<td v-if="visible('name')">
-			{{itemName}}
+			{{addonName}}
 			<textarea v-if="editable('description')"
 					  class="textarea"
-					  v-model="item.description"
+					  v-model="addon.description"
 					  rows="1"></textarea>
-			<font v-else>{{item.description}}</font>
-			<button v-if="snapshot.addons && snapshot.addons.length>0" type="button" class="button is-small" @click="$emit('new-addon')"> + Addon </button>
+			<font v-else>{{addon.description}}</font>
+			<button type="button" class="button is-small is-pulled-right has-text-danger" @click="removeAddon">
+				<i class="fal fa-times"></i>
+			</button>
 		</td>
 		<td v-if="visible('idlist')">
 			<div v-if="editable('idlist')">
-				<input v-if="item.quantity==1" class="input" v-model="idlist"/>
-				<textarea v-if="item.quantity>1 && item.quantity<=10" class="textarea" v-model="idlist"></textarea>
-				<input v-if="item.quantity>10" type="file" />
+				<input v-if="addon.quantity==1" class="input" v-model="addon.idlist"/>
+				<textarea v-if="addon.quantity>1 && addon.quantity<=10" class="textarea" v-model="addon.idlist"></textarea>
+				<input v-if="addon.quantity>10" type="file" />
 			</div>
 			<font v-else>{{idlist}}</font>
 		</td>
-		<td v-if="visible('period')">{{item.period}}</td>
+		<td v-if="visible('period')">{{addon.period}}</td>
 		<td v-if="visible('quantity')">
 			<b-numberinput v-if="editable('quantity')"
-						   v-model="item.quantity"
+						   v-model="addon.quantity"
 						   size="is-small"
 						   icon-pack="fal"></b-numberinput>
-			<font v-else>{{item.quantity}}</font>
+			<font v-else>{{addon.quantity}}</font>
 		</td>
 		<td v-if="visible('unit')" class="is-capitalized">
-			<b-select v-if="editable('unit')" v-model="item.unit" placeholder="Change unit" size="is-small">
+			<b-select v-if="editable('unit')" v-model="addon.unit" placeholder="Change unit" size="is-small">
 				<option v-for="(unit, u) in units" :value="unit" :key="u">{{unit}}</option>
 			</b-select>
 			<font v-else>{{unit}}</font>
 		</td>
 		<td v-if="visible('price')">
 			<b-numberinput v-if="editable('price')"
-						   v-model="item.price"
+						   v-model="addon.price"
 						   size="is-small"
 						   icon-pack="fal"
 						   step="0.01"></b-numberinput>
-			<font v-else>{{item.price}}</font>
+			<font v-else>{{addon.price}}</font>
 		</td>
 		<td v-if="visible('offer')">N/A</td>
 		<td v-if="visible('discount')">
 			<b-numberinput v-if="editable('discount')"
-						   v-model="item.discount"
+						   v-model="addon.discount"
 						   size="is-small"
 						   icon-pack="fal"
 						   step="0.01"></b-numberinput>
-			<font v-else>{{item.discount}} %</font>
+			<font v-else>{{addon.discount}} %</font>
 		</td>
 		<td v-if="visible('vat')">
 			<b-numberinput v-if="editable('vat')"
-						   v-model="item.vat.amount"
+						   v-model="addon.vat.amount"
 						   size="is-small"
 						   icon-pack="fal"
 						   step="0.01"></b-numberinput>
-			<font v-else>{{item.vat.amount}} %</font>
+			<font v-else>{{addon.vat.amount}} %</font>
 		</td>
-		<td v-if="visible('total') && show" class="has-text-right">{{item.price}}</td>
-		<td v-if="visible('totalVat')" class="has-text-right">{{item.price}}</td>
+		<td v-if="visible('total') && show" class="has-text-right">{{addon.price}}</td>
+		<td v-if="visible('totalVat')" class="has-text-right">
+			<b-numberinput v-if="editable('totalVat')"
+				v-model="addon.price"
+				size="is-small"
+				icon-pack="fal"
+				step="0.01"></b-numberinput>
+			<font v-else>{{addon.price}}</font>
+		</td>
 	</tr>
 	<tr v-else>
 		<td class="has-text-grey has-text-weight-light">{{counter}}</td>
 		<td>
 			<div class="is-inline-block">
-				<b-select @input="selection => $emit('set-addon', selection)" placeholder="Select Addon">
+				<b-select @input="setAddon" placeholder="Select Addon">
 					<option v-for="(addon, a) in addons" :value="addon" :key="a">
 						{{addon.label}}, ({{addon.price}}/{{addon.unit}})
 					</option>
 				</b-select>
 			</div>
 			<div class="is-inline-block" style="margin-left:15px;position:relative;top:5px;opacity:.6">
-				or <a href="#" class="has-text-danger" @click.prevent="deleteAddon">delete</a>
+				or <a href="#" class="has-text-danger" @click.prevent="removeAddon">delete</a>
 			</div>
 		</td>
 		<td colspan="20"><hr style="margin: 17px 0;background: #f0f0f0;height: 1px" /></td>
@@ -81,33 +90,12 @@
 	export default {
 		name: "InvoiceItemAddonRow",
 		props: {
-			vat: {
-				type: Object,
-				default() {
-					return {
-						amount: 18,
-						included: true
-					}
-				}
-			},
+			itemKey: { type: Number },
+			addonKey: { type: Number },
 			show: { type: Boolean },
+			addon: { type: Object },
 			fields: { type: String },
 			features: { type: String },
-			customer: {
-				type: Object,
-				default() {
-					return {
-						exclusivity: {
-							discount: 0
-						},
-						vat: {
-							amount: 18,
-							included: true
-						}
-					}
-				}
-			},
-			addon: { type: Object },
 			counter: { type: [ String, Number ] },
 			addons: { type: Array }
 		},
@@ -116,16 +104,30 @@
 			editable(column) {
 				return this.fields.includes(column+":edit") && !this.show;
 			},
+			setAddon(addon) {
+				this.$emit('set-addon', {
+					itemKey: this.itemKey,
+					key: this.addonKey,
+					value: addon
+				});
+			},
+			removeAddon() {
+				this.$emit('remove-addon', {
+					itemKey: this.itemKey,
+					key: this.addonKey,
+					value: null
+				});
+			}
 		},
 		computed: {
 			snapshot() { return this.addon && this.addon.snapshot ? this.addon.snapshot : null; },
 			addonName() {
-				var name = this.addon.name ?? this.snapshot.name;
+				var name = this.addon.name ?? this.snapshot.label;
 				if(this.addon.variant) name = `${name} ${this.addon.variant.label}`;
 				return name;
 			},
 			unit() {
-				var unit = (this.item && this.item.unit) ? this.item.unit : "Piece";
+				var unit = (this.addon && this.addon.unit) ? this.addon.unit : "Piece";
 				if(this.snapshot && this.snapshot.unit) unit = this.snapshot.unit;
 				return unit;
 			},
