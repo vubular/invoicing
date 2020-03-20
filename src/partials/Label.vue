74d6a196d8fbcfcf928015a5d414c50c00c83c6c
Mentor Gashi <m.gashi@albismart.com>
2020-03-20 18:37:29 +0100

    Accept show prop to toggle label class

--- src/partials/Label.vue
+++ src/partials/Label.vue
@@ -1,6 +1,6 @@
 <template>
 	<div v-if="showLabel" class="column">
-		<h3 class="subtitle is-3 has-text-grey-light has-text-weight-light">
+		<h3 class="subtitle is-3 has-text-weight-light" :class="stateClass">
 			<span class="icon"><i class="fal fa-th-large"></i></span>
 			<span v-html="label" style="margin-left:15px"></span>
 		</h3>
@@ -13,11 +13,17 @@
 			label: {
 				type: String,
 				default: "Invoice"
+			},
+			show: {
+				type: Boolean
 			}
 		},
 		computed: {
 			showLabel() {
 				return this.label != "hide";
+			},
+			stateClass() {
+				return this.show ? 'has-text-grey-light' : ''
 			}
 		}
 	}
