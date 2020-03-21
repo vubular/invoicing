26eece6b0bbb11ef2ae54390fce9cee88c307e4b
Mentor Gashi <m.gashi@albismart.com>
2020-03-21 23:08:39 +0100

    During install initializing filters: pricing, percentage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import Component from './VueInvoicing.vue';
 export default {
 
 	install(Vue, options) {
+
+		Vue.filter('pricing', function (value = null) {
+			value = +value;
+			if(!Number(value)) return "0.00 €";
+			if(value==null) return "0.00 €";
+			if(Number(value)==0) return "0.00 €";
+			return value ? value.toFixed(2) + " €" : "0.00 €";
+		});
+
+		Vue.filter('percentage', function (value) {
+			value = +value;
+			if (value==null && Number(value)!=value) return value;
+			if(Number(value)==0) return "N/A";
+			return value + "%";
+		});
+
 		Vue.component(Component.name, Component)
 	}
 
